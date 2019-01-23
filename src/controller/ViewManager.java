2fa97d491f219feb776bf24d1d9d15cdc5ff42f5
Justin Latorre <jlatorre2000@gmail.com>
2019-01-22 21:16:45 -0500

    finally

--- src/controller/ViewManager.java
+++ src/controller/ViewManager.java
@@ -2,13 +2,15 @@ package controller;
 
 import java.awt.CardLayout;
 import java.awt.Container;
-
 import javax.swing.JOptionPane;
-
 import data.Database;
 import model.BankAccount;
 import view.ATM;
+import view.HomeView;
 import view.LoginView;
+import view.WithdrawView;
+import view.InformationView;
+
 
 public class ViewManager {
 	
@@ -39,20 +41,103 @@ public class ViewManager {
 	 */
 	
 	public void login(String accountNumber, char[] pin) {
-		LoginView lv = ((LoginView) views.getComponents()[ATM.LOGIN_VIEW_INDEX]);
-		
 		try {
 			account = db.getAccount(Long.valueOf(accountNumber), Integer.valueOf(new String(pin)));
 			
-			if (account == null) {
+			if (account == null || account.getStatus() == 'N') {
+				LoginView lv = ((LoginView) views.getComponents()[ATM.LOGIN_VIEW_INDEX]);
 				lv.updateErrorMessage("Invalid account number and/or PIN.");
 			} else {
+				HomeView hv = ((HomeView) views.getComponents()[ATM.HOME_VIEW_INDEX]);
+				hv.setAccount(account);
 				switchTo(ATM.HOME_VIEW);
-				lv.clear();
+				
+				LoginView lv = ((LoginView) views.getComponents()[ATM.LOGIN_VIEW_INDEX]);
+				lv.updateErrorMessage("");
 			}
 		} catch (NumberFormatException e) {
-			lv.updateErrorMessage("Account numbers and PINs don't have letters.");
+			// ignore
 		}
+	}
+	public void showLabels() {
+		HomeView hv = ((HomeView) views.getComponents()[ATM.HOME_VIEW_INDEX]);
+		hv.initAccountNumber();
+	}
+	
+	public void closeAccount() {
+		db.closeAccount(account);
+		setAccount(null);
+		setDestination(null);
+		switchTo(ATM.LOGIN_VIEW);
+	}
+	
+	private void setDestination(BankAccount destination) {
+		this.destination = destination;
+	}
+
+	public void insertAccount(BankAccount account) {
+		db.insertAccount(account);
+	}
+	
+	public long newAccountNumber() {
+		return db.getMaxAccountNumber() + 1;
+	}
+	
+	
+	
+	public long getAccountNumber() {
+		try {
+			System.out.println(account.getAccountNumber());
+			return account.getAccountNumber();
+		}
+		catch (NullPointerException e) {
+			return -1;
+		}
+		
+	
+		
+	}
+	
+	public void initAccountNumber() {
+		HomeView hv = ((HomeView) views.getComponents()[ATM.HOME_VIEW_INDEX]);
+		hv.initAccountNumber();
+	}
+	
+	public void initFirstName() {
+		InformationView iv = ((InformationView) views.getComponents()[ATM.INFORMATION_VIEW_INDEX]);
+		iv.initInfo();
+	}
+	
+	
+	public int deposit(double amount) {
+		return account.deposit(amount);
+	}
+	
+	public int withdraw(double amount) {
+		return account.withdraw(amount);
+	}
+	public void transfer(long accountNumber, double amount) {
+		this.destination = db.getAccount(accountNumber);
+		account.transfer(destination, amount);
+		updateAccount(account);
+		updateAccount(destination);
+	}
+	public BankAccount getAccount(long accountNumber) {
+		return db.getAccount(accountNumber);
+	}
+	public void logout() {
+		int choice = JOptionPane.showConfirmDialog(
+			views,
+			"Are you sure?",
+			"Shutdown ATM",
+			JOptionPane.YES_NO_OPTION,
+			JOptionPane.QUESTION_MESSAGE
+		);
+			
+		if (choice == 0) {
+			account = null;
+			switchTo(ATM.LOGIN_VIEW);
+		}	
 	}
 	
 	/**
@@ -62,6 +147,11 @@ public class ViewManager {
 	 */
 	
 	public void switchTo(String view) {
+		if (view == ATM.INFORMATION_VIEW) {
+			InformationView iv = ((InformationView) views.getComponents()[ATM.INFORMATION_VIEW_INDEX]);
+			iv.setAccount(account);
+			iv.initInfo();
+		}
 		((CardLayout) views.getLayout()).show(views, view);
 	}
 	
@@ -87,5 +177,26 @@ public class ViewManager {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public BankAccount getAccount() {
+		return account;
+	}
+
+	public void setAccount(BankAccount account) {
+		this.account = account;
+	}
+	
+	public void setAccountInfo(BankAccount account) {
+		InformationView iv = ((InformationView) views.getComponents()[ATM.INFORMATION_VIEW_INDEX]);
+		iv.setAccount(account);
+	}
+	
+	public boolean updateAccount(BankAccount account) {
+		return db.updateAccount(account);
+	}
+	
+	public boolean updateDestinationAccount(BankAccount destination) {
+		return db.updateAccount(destination);
 	}
 }
